9456d5dd0de8550362ecf5ffe8b369ce830acb8e
Bradley Burgess <47909061+bradleyburgess@users.noreply.github.com>
2025-07-30 21:53:47 +0200

    feat: Add member to household (#7)
    * Add member to household
    
    * Add tests and validations
    
    - Check for unique household names
    - Check for unique household members

--- src/households/forms.py
+++ src/households/forms.py
@@ -7,3 +7,10 @@ class HouseholdCreateForm(forms.ModelForm):
     class Meta:
         model = Household
         fields = ("name",)
+
+
+class AddHouseholdMemberForm(forms.Form):
+    email = forms.EmailField(required=True, label="User Email")
+    is_admin = forms.BooleanField(
+        initial=False, label="Add as admin user", required=False
+    )
--- src/households/views.py
+++ src/households/views.py
@@ -1,12 +1,15 @@
 from uuid import UUID
 
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 
-from .forms import HouseholdCreateForm
+from .forms import HouseholdCreateForm, AddHouseholdMemberForm
 from .models import Household, HouseholdMember
+
+User = get_user_model()
 
 
 @login_required
@@ -26,15 +29,21 @@ def create(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
         form = HouseholdCreateForm(request.POST)
         if form.is_valid():
-            h = Household.objects.create(
-                name=form.cleaned_data["name"], created_by=request.user
-            )
-            HouseholdMember.objects.create(
-                household=h,
-                user=request.user,
-                member_type=HouseholdMember.MemberType.ADMIN,
-            )
-            return redirect(reverse_lazy("households:index"))
+            household_name = form.cleaned_data["name"].strip()
+            if HouseholdMember.objects.filter(
+                household__name=household_name, user=request.user
+            ):
+                form.add_error("name", "Household with this name already exists")
+            else:
+                h = Household.objects.create(
+                    name=form.cleaned_data["name"], created_by=request.user
+                )
+                HouseholdMember.objects.create(
+                    household=h,
+                    user=request.user,
+                    member_type=HouseholdMember.MemberType.ADMIN,
+                )
+                return redirect(reverse_lazy("households:index"))
     context = {"form": form}
     return render(request, "households/create.html", context=context)
 
@@ -57,3 +66,31 @@ def current_household_detail(request: HttpRequest) -> HttpResponse:
     members = HouseholdMember.objects.filter(household=household)
     context = {"household": household, "members": members}
     return render(request, "households/detail.html", context=context)
+
+
+@login_required
+def add_member(request: HttpRequest) -> HttpResponse:
+    form = AddHouseholdMemberForm()
+    household_uuid = request.session.get("current_household_uuid")
+    household = Household.objects.get(uuid=household_uuid)
+    if request.method == "POST":
+        form = AddHouseholdMemberForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            is_admin = form.cleaned_data["is_admin"]
+            try:
+                user_to_add = User.objects.get(email=email)
+                if HouseholdMember.objects.filter(
+                    user=user_to_add, household=household
+                ).exists():
+                    form.add_error("email", "User is already a household member")
+                else:
+                    hm = HouseholdMember(household=household, user=user_to_add)
+                    if is_admin:
+                        hm.member_type = HouseholdMember.MemberType.ADMIN
+                    hm.save()
+                    return redirect(reverse_lazy("households:view-current"))
+            except User.DoesNotExist:
+                form.add_error("email", "User does not exist.")
+    context = {"form": form, "household": household}
+    return render(request, "households/add_member.html", context=context)
