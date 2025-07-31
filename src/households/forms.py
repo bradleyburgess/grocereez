28b6a52ee88a0157f44aa80aa006265115224a91
Bradley Burgess <bradleyburgess@gmail.com>
2025-07-31 13:47:05 +0200

    Add `.strip` to `clean`

--- src/households/forms.py
+++ src/households/forms.py
@@ -1,9 +1,15 @@
+from typing import Any
 from django import forms
 
 from .models import Household
 
 
 class HouseholdCreateForm(forms.ModelForm):
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = super().clean()
+        cleaned_data["name"] = cleaned_data["name"].strip()
+        return cleaned_data
+
     class Meta:
         model = Household
         fields = ("name",)
